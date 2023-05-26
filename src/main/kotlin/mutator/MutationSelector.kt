93eb8285d650d54efc222ae2fc2e7b9129125939
Yi Lai <yl9419@ic.ac.uk>
2023-05-26 17:06:17 +0100

    implemented select One var decl Stmt

--- src/main/kotlin/mutator/MutationSelector.kt
+++ src/main/kotlin/mutator/MutationSelector.kt
@@ -1,17 +1,43 @@
 package mutator
 
-class MutationSelector {
+import ast.BlockStatement
+import ast.DafnyStatement
+import ast.VariableDeclarationStatement
+import utils.IRandom
+import utils.UnableToFindOriginalException
+
+class MutationSelector(
+    val rand: IRandom
+) {
     // all the Variable Declaration Statement Selected will be consecutive.
-    fun selectOneVarDeclStmt() {
+    fun selectOneVarDeclStmt(block: BlockStatement): MutationSubBlock {
+        val possibleOriginals = mutableListOf<Pair<Int, DafnyStatement>>()
+        for (i in block.statements.indices) {
+            val dafnyStmt = block.statements[i]
+            if (dafnyStmt.nonLabelStmt is VariableDeclarationStatement) {
+               possibleOriginals.add(Pair(i, dafnyStmt))
+            }
+        }
+        if (possibleOriginals.size == 0) {
+            throw UnableToFindOriginalException()
+        }
+        val (index, original) = possibleOriginals[rand.nextInt(possibleOriginals.size)]
+        return MutationSubBlock(index, 1, listOf(original), block)
+    }
+    fun selectTwoVarDeclStmt(block: BlockStatement) {
 
     }
-    fun selectTwoVarDeclStmt() {
 
-    }
-
-    fun selectThreeVarDeclStmt() {
+    fun selectThreeVarDeclStmt(block: BlockStatement) {
 
     }
 
 
 }
+
+data class MutationSubBlock(
+    val index: Int,
+    val arity: Int,
+    val statementsToMutate: List<DafnyStatement>,
+    val parent: BlockStatement
+)
