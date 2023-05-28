ab27a05b7eac5150605dc2c0603b5e4ef6e63eb2
Yi Lai <yl9419@ic.ac.uk>
2023-05-28 15:47:48 +0100

    added a mutationHelper, implemented method that chang one Stmt to if

--- src/main/kotlin/mutator/MutationHelper.kt
+++ src/main/kotlin/mutator/MutationHelper.kt
@@ -1,27 +1,71 @@
 package mutator
 
-class MutationHelper {
-    fun mutateOneStmtToIf() {
+import ast.BlockStatement
+import ast.DafnyStatement
+import ast.ElseSubStatement
+import ast.IfStatement
+import astGenerator.NaiveExprGenerator
+import astGenerator.genVarDeclWithoutRhs
+import utils.IRandom
 
+class MutationHelper(
+    val log: MutableList<String>,
+    val rand: IRandom,
+) {
+    val naive = NaiveExprGenerator(rand)
+    fun mutateOneStmtToIf(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 1 && statements.size == 1)
+        val stmtsInBlock = parent.statements
+        log.add("Removed ${statements[0].toDafny()}")
+        stmtsInBlock.removeAt(index)
+        stmtsInBlock.add(index, mutateToIf(statements, log))
+        stmtsInBlock.add(index, genVarDeclWithoutRhs(parent.stmtSymbolTable!!, statements[0], log))
     }
 
-    fun mutateTwoStmtToIf() {
-
+    fun mutateTwoStmtToIf(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 2 && statements.size == arity)
     }
 
-    fun mutateThreeStmtToIf() {
-
+    fun mutateThreeStmtToIf(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 3 && statements.size == arity)
     }
 
-    fun mutateOneStmtToFor() {
-
+    fun mutateOneStmtToFor(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 1 && statements.size == arity)
     }
 
-    fun mutateTwoStmtToFor() {
-
+    fun mutateTwoStmtToFor(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 2 && statements.size == arity)
     }
 
-    fun mutateThreeStmtToFor() {
-        
+    fun mutateThreeStmtToFor(mutBlock: MutationSubBlock) {
+        val (index, arity, statements, parent) = mutBlock
+        assert(arity == 3 && statements.size == arity)
+    }
+
+    private fun mutateToIf(
+        statements: List<DafnyStatement>,
+        log: MutableList<String>
+    ): DafnyStatement {
+        val st = statements[0].stmtSymbolTable!!
+        val nonLabel = if (rand.nextBoolean()) {
+            //true
+            val guard = naive.genDafnyExpression(result = true, st)
+            val ifStmt = IfStatement(guard, BlockStatement(statements.toMutableList(), 99), null)
+            log.add("Changed to ${ifStmt.toDafny()}")
+            ifStmt
+        } else {
+            val guard = naive.genDafnyExpression(result = false, st)
+            val elseBlock = ElseSubStatement(BlockStatement(statements.toMutableList(), 99))
+            val ifStmt = IfStatement(guard, BlockStatement(mutableListOf(), 99), elseBlock)
+            log.add("Changed to ${ifStmt.toDafny()}")
+            ifStmt
+        }
+        return DafnyStatement(null, nonLabel)
     }
 }
