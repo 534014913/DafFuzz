34589812b2e1376ad7c907283b90572f2beb1eb9
Yi Lai <yl9419@ic.ac.uk>
2023-05-28 17:04:16 +0100

    added a ForStatement

--- src/main/kotlin/ast/StatementNode.kt
+++ src/main/kotlin/ast/StatementNode.kt
@@ -228,7 +228,7 @@ data class PrintStatement(
     }
 
     override fun walk(st: SymbolTable, walker: DafnyWalker) {
-        stmtSymbolTable = st;
+        stmtSymbolTable = st
     }
 }
 
@@ -326,5 +326,30 @@ data class AssumeStatement(
 
     override fun walk(st: SymbolTable, walker: DafnyWalker) {
         stmtSymbolTable = st.clone()
+    }
+}
+
+data class ForStatement(
+    val isTo: Boolean,
+    val leftBound: Int,
+    val rightBound: Int,
+    val block: BlockStatement,
+    override var stmtSymbolTable: SymbolTable? = null
+) : StatementNode {
+    override fun clone(): StatementNode {
+        return this.copy(block = block.clone())
+    }
+
+    override fun toDafny(): String {
+        if (isTo) {
+            assert(leftBound < rightBound)
+        } else {
+            assert(leftBound > rightBound)
+        }
+        return "for x := $leftBound ${if (isTo) "to" else "downto"} $rightBound ${block.toDafny()}"
+    }
+
+    override fun walk(st: SymbolTable, walker: DafnyWalker) {
+        throw RuntimeException("for statement is not walked as it should not exist at after parsing dafny")
     }
 }
