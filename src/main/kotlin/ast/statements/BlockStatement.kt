96de215976c299db9324a04232de9e56a41aee9d
Yi Lai <yl9419@ic.ac.uk>
2023-06-04 14:40:29 +0100

    added InjectionPoint interface and class from graphicsFuzz project to provide a more unified interface for mutation

--- src/main/kotlin/ast/statements/BlockStatement.kt
+++ src/main/kotlin/ast/statements/BlockStatement.kt
@@ -20,6 +20,38 @@ data class BlockStatement(
         printIdent = true
     }
 
+    fun insertStmt(index: Int, stmt: DafnyStatement) {
+        assert(index <= statements.size)
+        statements.add(index, stmt)
+    }
+
+    fun insertBefore(original: DafnyStatement, inserted: DafnyStatement) {
+        for (i in statements.indices) {
+           if (statements[i] == original) {
+               insertStmt(i, inserted)
+               return
+           }
+        }
+        throw RuntimeException("Can't find original statement in the block")
+    }
+
+    fun getSize(): Int {
+        return statements.size
+    }
+
+    fun getIndices(): IntRange {
+        return statements.indices
+    }
+
+    fun getStmt(i: Int): DafnyStatement {
+        assert(i < statements.size)
+        return statements[i]
+    }
+
+    fun setStmt(index: Int, stmt: DafnyStatement) {
+        statements.set(index, stmt)
+    }
+
     override fun clone(): BlockStatement {
         return BlockStatement(
             statements.map { it.clone() }.toMutableList(),
---
+++ src/main/kotlin/mutator/injectionPoint/BlockInjectionPoint.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 The GraphicsFuzz Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mutator.injectionPoint
+
+import ast.method.MethodDeclaration
+import ast.statements.BlockStatement
+import ast.statements.DafnyStatement
+import ast.symbolTable.SymbolTable
+
+class BlockInjectionPoint(
+    val block: BlockStatement,
+    val nextStmt: DafnyStatement?,
+    methodDecl: MethodDeclaration,
+    inLoop: Boolean,
+    symbolTable: SymbolTable
+): InjectionPoint(
+    methodDecl, inLoop, symbolTable
+) {
+    override fun inject(stmt: DafnyStatement) {
+        if (nextStmt == null) {
+            block.insertStmt(block.getSize(), stmt)
+        } else {
+            block.insertBefore(nextStmt, stmt)
+        }
+    }
+
+    override fun hasNextStmt(): Boolean {
+        return nextStmt != null
+    }
+
+    override fun getNextStmt(): DafnyStatement? {
+        assert(hasNextStmt())
+        return nextStmt
+    }
+
+    override fun replaceNextStmt(stmt: DafnyStatement) {
+        assert(hasNextStmt())
+        for (i in block.getIndices()) {
+            if (block.getStmt(i) == nextStmt) {
+                block.setStmt(i, stmt)
+            }
+        }
+    }
+}
